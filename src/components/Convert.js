3cc467ee012aa1704e3fd681dafd20e1eae02e1a
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-26 13:11:06 -0700

    Convert now displays translated text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,20 +4,32 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_TRANSLATE_API_KEY;
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+
   useEffect(() => {
-    axios.post(
-      `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
   }, [language, text]);
 
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;

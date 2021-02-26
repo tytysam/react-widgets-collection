0a392e36e306b92a42ac66cdc2b534f8906f93ae
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-26 16:56:23 -0700

    Constructed navigation event to detect navigation and update routes

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,11 @@ import React from "react";
 const Link = ({ className, href, children }) => {
   const onClick = (e) => {
     e.preventDefault();
+
+    window.history.pushState({}, "", href);
+
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
 
   return (

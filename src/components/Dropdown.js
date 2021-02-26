6c43d80736281fa628de28529ae3d5553a4b56ce
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-26 11:54:06 -0700

    Utillized useRef to make my dropdown closeable by clicking outside of the Dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener("click", (e) => {
+      if (ref.current && ref.current.contains(e.target)) {
+        return;
+      }
+
+      setOpen(false);
+    });
+  }, []);
+
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -20,13 +33,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"> </i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>

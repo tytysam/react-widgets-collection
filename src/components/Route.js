9217d3bdaa361f146a5872ac8d8ffc101de2b457
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-26 16:59:25 -0700

    Buttoned-up basic Route and Link components

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 
 const Route = ({ path, children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   useEffect(() => {
     const onLocationChange = () => {
-      console.log("Location change");
+      setCurrentPath(window.location.pathname);
     };
 
     window.addEventListener("popstate", onLocationChange);
@@ -13,7 +15,7 @@ const Route = ({ path, children }) => {
     };
   }, []);
 
-  return window.location.pathname === path ? children : null;
+  return currentPath === path ? children : null;
 };
 
 export default Route;
